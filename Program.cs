f35f866c4a7963b8f6ff0a356b9319649d80aff0
tarytsa <a.tarytsa@gmail.com>
2020-06-03 11:35:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mirror_Word
+{
+    delegate string Mirror(string a);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mirror a1 = delegate (string a)
+            {
+                char[] b = a.ToCharArray();
+                Array.Reverse(b);
+                return new string(b);
+            };
+            Console.WriteLine(a1("Hello world"));
+            Console.WriteLine(a1("123"));
+            
+        }
+    }
+}
